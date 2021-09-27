21118e4eb66dd1ffafc51cd5f680339d657f8dd5
Rushiraj11 <grushiraj95@gmail.com>
2021-09-27 17:54:18 +0530

    Added:Welcome Message

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/model/CabInvoiceGeneratorMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.cabinvoicegenerator.model;
+
+public class CabInvoiceGeneratorMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to CabInvoice Generator");
+    }
+}
